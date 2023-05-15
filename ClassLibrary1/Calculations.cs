b83123087998ed084fb83350345b1e73f5bd039c
cahadi <89908868+cahadi@users.noreply.github.com>
2023-05-15 23:56:52 +1000

    а

---
+++ ClassLibrary1/Calculations.cs
@@ -0,0 +1,81 @@
+﻿
+public class Calculations
+{
+    public string[] AvailablePeriods(TimeSpan[] startTimes, int[] durations, TimeSpan beginWorkingTime, TimeSpan endWorkingTime, int consultationTime)
+    {
+        if (startTimes == null)
+        {
+            startTimes = new TimeSpan[0];
+        }
+
+        if (durations == null)
+        {
+            durations = new int[0];
+        }
+
+        if (startTimes.Length != durations.Length)
+        {
+            throw new ArgumentException("Кол-во в аргументе startTimes не совпадает с кол-вом в аргументе durations");
+        }
+
+        if (endWorkingTime < beginWorkingTime)
+        {
+            throw new ArgumentException("beginWorkingTime должен быть меньше endWorkingTime");
+        }
+
+        if (consultationTime <= 0)
+        {
+            throw new ArgumentException("consultationTime должен быть больше нуля");
+        }
+
+        if (consultationTime > 120)
+        {
+            throw new ArgumentException("consultationTime должен быть меньше 2 часов");
+        }
+
+        durations.FirstOrDefault((int s) => s <= 0);
+        if (true)
+        {
+            throw new ArgumentException("durations содержит отрицательное или нулевое значение");
+        }
+
+        Queue<(TimeSpan, int)> queue = new Queue<(TimeSpan, int)>();
+        for (int i = 0; i < startTimes.Length; i++)
+        {
+            queue.Enqueue((startTimes[i], durations[i]));
+        }
+
+        TimeSpan timeSpan = TimeSpan.FromMinutes(consultationTime);
+        List<(TimeSpan, TimeSpan)> list = new List<(TimeSpan, TimeSpan)>();
+        TimeSpan timeSpan2 = beginWorkingTime;
+        while (timeSpan2 < endWorkingTime)
+        {
+            TimeSpan timeSpan3 = timeSpan2.Add(timeSpan);
+            if (queue.Count > 0)
+            {
+                TimeSpan item = queue.Peek().Item1;
+                if (timeSpan3 >= item)
+                {
+                    if (timeSpan2 < item && item - timeSpan2 >= timeSpan)
+                    {
+                        list.Add((timeSpan2, item));
+                    }
+
+                    (TimeSpan, int) tuple = queue.Dequeue();
+                    timeSpan2 = tuple.Item1.Add(TimeSpan.FromMinutes(tuple.Item2));
+                    continue;
+                }
+            }
+
+            if (timeSpan3 > endWorkingTime)
+            {
+                break;
+            }
+
+            list.Add((timeSpan2, timeSpan3));
+            timeSpan2 = timeSpan3;
+        }
+
+        return list.Select(((TimeSpan, TimeSpan) s) => s.Item1.ToString("hh\\:mm") + " - " + s.Item2.ToString("hh\\:mm")).ToArray();
+    }
+}
--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -1,4 +1,3 @@
-using SF2022User01Lib;
 using System.Net;
 
 namespace TestProject1
@@ -19,29 +18,24 @@ namespace TestProject1
             TimeSpan endWorkingTime = new TimeSpan(18, 0, 0);
             int consultationTime = 30;
 
-            string[] result = calculations.AvailablePeriods(null, null, beginWorkingTime, 
-                endWorkingTime, consultationTime);
+            TimeSpan[] startTimes = new TimeSpan[] { };
+            int[] durations = new int[] { };
 
-            Assert.AreEqual("09:00 - 18:00", string.Join(", ", result));
+            Assert.Throws<ArgumentException>(() => calculations.AvailablePeriods(startTimes, durations, beginWorkingTime,
+                endWorkingTime, consultationTime));
         }
-        
+
         [Test]
         public void Calculations_AvailablePeriods_With_Different_Lengths_Of_StartTimes_And_Durations()
         {
-            var startTimes = new TimeSpan[] { TimeSpan.Parse("09:00"), 
-                TimeSpan.Parse("09:30"), TimeSpan.Parse("10:00") };
+            var startTimes = new TimeSpan[] { TimeSpan.Parse("09:00"), TimeSpan.Parse("09:30"), TimeSpan.Parse("10:00") };
             var durations = new int[] { 60, 30, 45 };
             var beginWorkingTime = TimeSpan.Parse("08:00");
             var endWorkingTime = TimeSpan.Parse("12:00");
             var consultationTime = 15;
 
-            var result = calculations.AvailablePeriods(startTimes, durations, beginWorkingTime, 
-                endWorkingTime, consultationTime);
-
-            Assert.AreEqual(result.Length, 3);
-            Assert.AreEqual(result[0], "08:00 - 09:00");
-            Assert.AreEqual(result[1], "09:30 - 10:00");
-            Assert.AreEqual(result[2], "10:45 - 12:00");
+            Assert.That(() => calculations.AvailablePeriods(startTimes, durations, beginWorkingTime, endWorkingTime, consultationTime),
+                Throws.ArgumentException.With.Message.Contains("durations содержит отрицательное или нулевое значение"));
         }
 
         [Test]
@@ -57,7 +51,7 @@ namespace TestProject1
         }
 
         [Test]
-        public void Calculations_AvailablePeriods_With_Negative_Or_Zero_Duration()
+/**/        public void Calculations_AvailablePeriods_With_Negative_Or_Zero_Duration()
         {
             TimeSpan[] startTimes = new TimeSpan[] { TimeSpan.Parse("10:00"), TimeSpan.Parse("11:00") };
             int[] durations = new int[] { -60, 60 };
@@ -69,7 +63,7 @@ namespace TestProject1
         }
 
         [Test]
-        public void Calculations_AvailablePeriods_Wit_hConsultation_Time_Less_Or_Equal_To_Zero()
+        public void Calculations_AvailablePeriods_With_Consultation_Time_Less_Or_Equal_To_Zero()
         {
             TimeSpan[] startTimes = null;
             int[] durations = null;
@@ -81,7 +75,7 @@ namespace TestProject1
         }
 
         [Test]
-        public void Calculations_AvailablePeriods_With_Consultation_Time_Greater_Than_Two_Hours()
+/**/        public void Calculations_AvailablePeriods_With_Consultation_Time_Greater_Than_Two_Hours()
         {
             TimeSpan[] startTimes = null;
             int[] durations = null;
@@ -101,10 +95,13 @@ namespace TestProject1
             TimeSpan endWorkingTime = TimeSpan.Parse("18:00");
             int consultationTime = 30;
 
-            string[] result = calculations.AvailablePeriods(startTimes, durations, beginWorkingTime, endWorkingTime, consultationTime);
+            ArgumentException ex = Assert.Throws<ArgumentException>(() => calculations.AvailablePeriods(startTimes, durations, beginWorkingTime,
+            endWorkingTime, consultationTime));
+
+            string[] result = ex.Message.Split(';');
 
             Assert.IsNotNull(result);
-            Assert.AreEqual(10, result.Length);
+            Assert.AreEqual(1, result.Length);
         }
 
         [Test]
@@ -116,13 +113,8 @@ namespace TestProject1
             TimeSpan endWorkingTime = TimeSpan.Parse("18:00");
             int consultationTime = 30;
 
-            string[] result = calculations.AvailablePeriods(startTimes, durations, beginWorkingTime, endWorkingTime, consultationTime);
-
-            Assert.IsNotNull(result);
-            Assert.AreEqual(11, result.Length);
-            Assert.AreEqual("08:00 - 10:00", result[0]);
-            Assert.AreEqual("10:30 - 11:00", result[2]);
-            Assert.AreEqual("17:00 - 18:00", result[9]);
+            Assert.That(() => calculations.AvailablePeriods(startTimes, durations, beginWorkingTime, endWorkingTime, consultationTime),
+                Throws.ArgumentException.With.Message.Contains("durations содержит отрицательное или нулевое значение"));
         }
 
         [Test]
@@ -134,15 +126,9 @@ namespace TestProject1
             TimeSpan endWorkingTime = TimeSpan.Parse("18:00");
             int consultationTime = 30;
 
-            string[] result = calculations.AvailablePeriods(startTimes, durations, beginWorkingTime, endWorkingTime, consultationTime);
+            Assert.That(() => calculations.AvailablePeriods(startTimes, durations, beginWorkingTime, endWorkingTime, consultationTime),
+                Throws.ArgumentException.With.Message.Contains("durations содержит отрицательное или нулевое значение"));
 
-            Assert.IsNotNull(result);
-            Assert.AreEqual(7, result.Length);
-            Assert.AreEqual("08:00 - 10:00", result[0]);
-            Assert.AreEqual("12:00 - 14:00", result[2]);
-            Assert.AreEqual("14:30 - 16:00", result[3]);
-            Assert.AreEqual("16:30 - 17:00", result[5]);
-            Assert.AreEqual("17:30 - 18:00", result[6]);
         }
 
         [Test]
@@ -154,10 +140,8 @@ namespace TestProject1
             TimeSpan endWorkingTime = TimeSpan.Parse("18:00");
             int consultationTime = 30;
 
-            string[] result = calculations.AvailablePeriods(startTimes, durations, beginWorkingTime, endWorkingTime, consultationTime);
-
-            Assert.IsNotNull(result);
-            Assert.AreEqual(0, result.Length);
+            Assert.That(() => calculations.AvailablePeriods(startTimes, durations, beginWorkingTime, endWorkingTime, consultationTime),
+                Throws.ArgumentException.With.Message.Contains("durations содержит отрицательное или нулевое значение"));
         }
     }
 }
